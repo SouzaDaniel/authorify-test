556728d8943bc91548590575a544bcac220faf9c
souzadaniel <eu@danielsouza.dev.br>
2023-02-02 03:32:58 -0300

    feat(setup): concluding setup project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { FormProvider } from './store/providers/Form';
 
-export default App;
+import { Page } from './pages/Home';
+
+import { GlobalStyles } from './styles/global';
+import { theme } from './styles/theme';
+
+export const App = () => (
+  <ThemeProvider theme={theme}>
+    <FormProvider>
+      <GlobalStyles />
+      <Page />
+    </FormProvider>
+  </ThemeProvider>
+);
